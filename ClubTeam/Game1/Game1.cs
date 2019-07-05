d51e6db7c361144f6bc57c470d8c0452dbbd5f1a
bondy-4869 <k019g1508.mizutani@gmail.com>
2019-07-05 18:02:18 +0900

    Revert "aaa"
    This reverts commit e9ddbdafa40feeba446fd81d4c97c692c33b2c76.

---
+++ ClubTeam/Game1/Game1.cs
@@ -0,0 +1,109 @@
+﻿// このファイルで必要なライブラリのnamespaceを指定
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+/// <summary>
+/// プロジェクト名がnamespaceとなります
+/// </summary>
+namespace Game1
+{
+    /// <summary>
+    /// ゲームの基盤となるメインのクラス
+    /// 親クラスはXNA.FrameworkのGameクラス
+    /// </summary>
+    public class Game1 : Game
+    {
+        // フィールド（このクラスの情報を記述）
+        private GraphicsDeviceManager graphicsDeviceManager;//グラフィックスデバイスを管理するオブジェクト
+        private SpriteBatch spriteBatch;//画像をスクリーン上に描画するためのオブジェクト
+
+        /// <summary>
+        /// コンストラクタ
+        /// （new で実体生成された際、一番最初に一回呼び出される）
+        /// </summary>
+        public Game1()
+        {
+            //グラフィックスデバイス管理者の実体生成
+            graphicsDeviceManager = new GraphicsDeviceManager(this);
+            //コンテンツデータ（リソースデータ）のルートフォルダは"Contentに設定
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// 初期化処理（起動時、コンストラクタの後に1度だけ呼ばれる）
+        /// </summary>
+        protected override void Initialize()
+        {
+            // この下にロジックを記述
+
+
+
+            // この上にロジックを記述
+            base.Initialize();// 親クラスの初期化処理呼び出し。絶対に消すな！！
+        }
+
+        /// <summary>
+        /// コンテンツデータ（リソースデータ）の読み込み処理
+        /// （起動時、１度だけ呼ばれる）
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // 画像を描画するために、スプライトバッチオブジェクトの実体生成
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // この下にロジックを記述
+
+
+            // この上にロジックを記述
+        }
+
+        /// <summary>
+        /// コンテンツの解放処理
+        /// （コンテンツ管理者以外で読み込んだコンテンツデータを解放）
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // この下にロジックを記述
+
+
+            // この上にロジックを記述
+        }
+
+        /// <summary>
+        /// 更新処理
+        /// （1/60秒の１フレーム分の更新内容を記述。音再生はここで行う）
+        /// </summary>
+        /// <param name="gameTime">現在のゲーム時間を提供するオブジェクト</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // ゲーム終了処理（ゲームパッドのBackボタンかキーボードのエスケープボタンが押されたら終了）
+            if ((GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed) ||
+                 (Keyboard.GetState().IsKeyDown(Keys.Escape)))
+            {
+                Exit();
+            }
+
+            // この下に更新ロジックを記述
+
+            // この上にロジックを記述
+            base.Update(gameTime); // 親クラスの更新処理呼び出し。絶対に消すな！！
+        }
+
+        /// <summary>
+        /// 描画処理
+        /// </summary>
+        /// <param name="gameTime">現在のゲーム時間を提供するオブジェクト</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            // 画面クリア時の色を設定
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // この下に描画ロジックを記述
+
+
+            //この上にロジックを記述
+            base.Draw(gameTime); // 親クラスの更新処理呼び出し。絶対に消すな！！
+        }
+    }
+}
