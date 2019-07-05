dd6d1c220be095badc258f9151d0fa5c6e03bc69
bondy-4869 <k019g1508.mizutani@gmail.com>
2019-07-05 18:13:53 +0900

    新しくプロジェクトを作り直しました

---
+++ Game1/Game1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Game1
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// メインクラス
+    /// （ゲームプログラムの実体生成と実行を行っている）
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// プロジェクトで一番最初に動くメソッド Main
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // ゲームオブジェクトの実体生成
+            using (var game = new Game1())
+                game.Run(); //ゲームの実行
+        }
+    }
+#endif
+}
